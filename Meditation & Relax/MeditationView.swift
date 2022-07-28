ddc179ba8104f11d977666388433b3b01eb9d721
Amjad Oudeh <74506206+Amjadoudeh@users.noreply.github.com>
2022-07-28 16:26:36 +0200

    Styling the Type and Duration

--- Meditation & Relax/MeditationView.swift
+++ Meditation & Relax/MeditationView.swift
@@ -3,17 +3,27 @@ import SwiftUI
 struct MeditationView: View {
     var body: some View {
         VStack(spacing: 0) {
-            // MARK: Image
+            // MARK: Image:
             Image("yoga")
                 .resizable()
                 .scaledToFill()
             .frame(height: UIScreen.main.bounds.height/3)
             
-            // MARK: Meditation Details
+            // MARK: Meditation Details:
             ZStack {
                 // MARK: Background
                 Color(red: 24/355, green: 23/255, blue: 22/255  )
                     .frame(height: UIScreen.main.bounds.height * 2/3)
+            
+                // MARK: Type and Duration:
+                VStack(alignment: .leading, spacing: 24) {
+                    VStack(alignment: .leading, spacing: 8) {
+                        Text("Music")
+                        
+                        Text("0s")
+                    }
+                    .foregroundColor(.white)
+                }
             }
         }
         .ignoresSafeArea()

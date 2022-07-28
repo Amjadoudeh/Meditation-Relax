2f676ecf06039763d63741cabf75e7235cc622d4
Amjad Oudeh <74506206+Amjadoudeh@users.noreply.github.com>
2022-07-28 23:04:35 +0200

    Styling the playerview screen

--- Meditation & Relax/View/PlayView.swift
+++ Meditation & Relax/View/PlayView.swift
@@ -10,11 +10,36 @@ import SwiftUI
 struct PlayView: View {
     var body: some View {
         ZStack {
+            // MARK: Background Image
             Image("yoga")
                 .resizable()
                 .scaledToFill()
                 .frame(width: UIScreen.main.bounds.width)
                 .ignoresSafeArea()
+            
+            // MARK: Dismiss Button
+            VStack(spacing: 32) {
+                HStack{
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "xmark.circle.fill")
+                            .font(.system(size: 36))
+                            
+                    }
+                    Spacer()
+                   
+                }
+                .padding(20)
+                .foregroundColor(.white)
+                
+                // MARK: Title
+                Text("1 Minute Relx and Meditation")
+                    .font(.title)
+                    .foregroundColor(.white)
+                
+                Spacer()
+            }
         }
     }
 }

63f2e26d72f5fe58281ffdfd0d8b609f7919e13d
Amjad Oudeh <74506206+Amjadoudeh@users.noreply.github.com>
2022-07-28 16:22:41 +0200

    Add a bg for the Details section

--- Meditation & Relax/MeditationView.swift
+++ Meditation & Relax/MeditationView.swift
@@ -2,10 +2,21 @@ import SwiftUI
 
 struct MeditationView: View {
     var body: some View {
-        Image("yoga")
-            .resizable()
-            .scaledToFill()
+        VStack(spacing: 0) {
+            // MARK: Image
+            Image("yoga")
+                .resizable()
+                .scaledToFill()
             .frame(height: UIScreen.main.bounds.height/3)
+            
+            // MARK: Meditation Details
+            ZStack {
+                // MARK: Background
+                Color(red: 24/355, green: 23/255, blue: 22/255  )
+                    .frame(height: UIScreen.main.bounds.height * 2/3)
+            }
+        }
+        .ignoresSafeArea()
     }
 }
 

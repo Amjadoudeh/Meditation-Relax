3bc2f3154da37e24bde293d11fe98ac6e4d334e0
Amjad Oudeh <74506206+Amjadoudeh@users.noreply.github.com>
2022-07-29 20:08:35 +0200

    Create a PlayBackControlButton

---
+++ Meditation & Relax/View/PlayBackControlButton.swift
@@ -0,0 +1,31 @@
+//
+//  PlayControllVButton.swift
+//  Meditation & Relax
+//
+//  Created by Amjad Oudeh on 29.07.22.
+//
+
+import SwiftUI
+
+struct PlayBackControlButton: View {
+    var systemName: String = "play"
+    var fontSize: CGFloat = 24
+    var color: Color = .white
+    var action: () -> Void
+    var body: some View {
+        Button {
+            action()
+        } label: {
+            Image(systemName: systemName)
+                .font(.system(size: fontSize))
+                .foregroundColor(color)
+        }
+    }
+}
+
+struct PlayControllVButton_Previews: PreviewProvider {
+    static var previews: some View {
+        PlayBackControlButton(action: {})
+            .preferredColorScheme(.dark)
+    }
+}

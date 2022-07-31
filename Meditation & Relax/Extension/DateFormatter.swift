1e35af4b66107205ad80efe07bac58bc048639a5
Amjad Oudeh <74506206+Amjadoudeh@users.noreply.github.com>
2022-07-31 18:37:42 +0200

    Create a DateComponentFormatter as extension

---
+++ Meditation & Relax/Extension/DateFormatter.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+extension DateComponentsFormatter {
+    static let abbreviated: DateComponentsFormatter = {
+        print("Initiatlising DateComponentsFormatter.abbreviated")
+        let formatter = DateComponentsFormatter()
+        
+        formatter.allowedUnits = [.hour, .minute, .second]
+        formatter.unitsStyle = .abbreviated
+        
+        return formatter
+    }()
+    
+    static let positional: DateComponentsFormatter = {
+        print("Initiatlising DateComponentsFormatter.positional")
+        let formatter = DateComponentsFormatter()
+        
+        formatter.allowedUnits = [.minute, .second]
+        formatter.unitsStyle = .positional
+        formatter.zeroFormattingBehavior = .pad
+        
+        return formatter
+    }()
+}

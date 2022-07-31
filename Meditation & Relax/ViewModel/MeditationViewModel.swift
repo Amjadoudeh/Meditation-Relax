fddc93608ec4a9b09ebb1688813c238b40fa6cbc
Amjad Oudeh <74506206+Amjadoudeh@users.noreply.github.com>
2022-07-31 17:51:01 +0200

    Create a MeditationViewModel

---
+++ Meditation & Relax/ViewModel/MeditationViewModel.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+struct Meditation {
+    let id = UUID()
+    let title: String
+    let description: String
+    let duration: TimeInterval
+    let track: String
+    let image: String
+}

fac0a24cb02782c3bb6beaeb4d95bfe656e11d0a
Amjad Oudeh <74506206+Amjadoudeh@users.noreply.github.com>
2022-08-01 09:33:37 +0200

    Add a condition to controle the previewer play

--- Meditation & Relax/Model/AudioManager.swift
+++ Meditation & Relax/Model/AudioManager.swift
@@ -1,11 +1,11 @@
 import Foundation
 import AVKit
 
-final class AudioManager {
+final class AudioManager: ObservableObject {
     static let shared = AudioManager()
     var player: AVAudioPlayer?
     
-    func startPlayer(track: String) {
+    func startPlayer(track: String, isPreview: Bool = false) {
         guard let url = Bundle.main.url(forResource: track, withExtension: "mp3") else {
             print("Resource not found \(track)")
             return
@@ -15,7 +15,13 @@ final class AudioManager {
             try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
             try AVAudioSession.sharedInstance().setActive(true)
             player = try AVAudioPlayer(contentsOf: url)
-            player?.play()
+            
+            if isPreview {
+                player?.prepareToPlay()
+            } else {
+                player?.play()
+            }
+            
         } catch {
             print("Fail to initialize the player", error)
         }
--- Meditation & Relax/View/PlayView.swift
+++ Meditation & Relax/View/PlayView.swift
@@ -2,7 +2,7 @@ import SwiftUI
 
 struct PlayView: View {
     var meditationVM: MeditationViewModel
-    
+    var isPreview: Bool = false
     @State private var value: Double = 0.0
     @Environment(\.dismiss) var dismiss
     var body: some View {
@@ -89,14 +89,14 @@ struct PlayView: View {
             }
         }
         .onAppear{
-            AudioManager.shared.startPlayer(track: meditationVM.meditation.track)
+            AudioManager.shared.startPlayer(track: meditationVM.meditation.track, isPreview: isPreview)
         }
     }
     
     struct PlayView_Previews: PreviewProvider {
         static let meditationVM = MeditationViewModel(meditation: Meditation.data)
         static var previews: some View {
-            PlayView(meditationVM: meditationVM)
+            PlayView(meditationVM: meditationVM, isPreview: true)
         }
     }
 }
